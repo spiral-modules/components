0e3ce39a73d6592f14f550d2c8c4f1580a01d8c5
Wolfy-J <wolfy.jd@gmail.com>
2015-07-26 23:27:03 +0300

    better tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Spiral Framework, SpiralScout LLC.
+ *
+ * @package   spiralFramework
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+define('SPIRAL_INITIAL_TIME', microtime(true));
+
+/**
+ * Error reporting.
+ */
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', true);
+
+//Composer
+require dirname(__DIR__) . '/vendor/autoload.php';

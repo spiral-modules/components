95b528ec97d19acb092db6c4f141337c8f95d81b
Wolfy-J <wolfy.jd@gmail.com>
2015-07-29 01:12:17 +0300

    No need in bundle anymore.

--- source/Translator/Traits/TranslatorTrait.php
+++ source/Translator/Traits/TranslatorTrait.php
@@ -24,30 +24,6 @@ trait TranslatorTrait
     abstract public function getContainer();
 
     /**
-     * Models and other classes which uses TranslatorTrait interface allowed to be automatically
-     * parsed and analyzed for messages stored in default property values (static and non static),
-     * such values can be prepended and appended with i18n prefixes ([[ and ]] by default) and will
-     * be localized on output. Class should implement i18nBundle method (static) which will define
-     * required i18n namespace. Namespace will be used to translate default messages and by i18n
-     * component to index this messages to localization bundles.
-     *
-     * Class should will be responsible by itself to localize such messages. Use "@do-not-index" doc
-     * comment to prevent field from indexation.
-     *
-     * Additionally method translate() introduced, this method can be used to localize default
-     * model messages from attributes ([[ and ]] will be cut) or be called directly in one of model
-     * method. Both usages will be indexed and captured to bundles.
-     *
-     * If you want to index both local and parent messages, set constant I18N_INHERIT_MESSAGES to true.
-     *
-     * @return string
-     */
-    public static function i18nBundle()
-    {
-        return get_called_class();
-    }
-
-    /**
      * Perform automatic message localization. Messages with [[ ]] and without braces accepted.
      * Please use this method statically as in this case it will be correctly indexed. Method will
      * work only with global container is set.
@@ -76,6 +52,6 @@ trait TranslatorTrait
             $string = substr($string, 2, -2);
         }
 
-        return $container->get(TranslatorInterface::class)->translate(static::i18nBundle(), $string);
+        return $container->get(TranslatorInterface::class)->translate(static::class, $string);
     }
 }

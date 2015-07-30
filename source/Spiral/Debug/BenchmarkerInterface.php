f9a06e22e9de11d33e929c92e2e7056f30bd8fb1
Wolfy-J <wolfy.jd@gmail.com>
2015-07-30 18:28:14 +0300

    benchmarker trait

--- source/Spiral/Debug/BenchmarkerInterface.php
+++ source/Spiral/Debug/BenchmarkerInterface.php
@@ -8,12 +8,14 @@
  */
 namespace Spiral\Debug;
 
+/**
+ * Interface responsible for benchmarking.
+ */
 interface BenchmarkerInterface
 {
     /**
-     * Benchmarks used to record long or important operations inside spiral components. Only time
-     * will be recorded. Method should return elapsed time when record will be closed (same set of
-     * arguments has to be provided).
+     * Benchmarks used to record long or important operations inside spiral components. Method should
+     * return elapsed time when record are be closed (same set of arguments has to be provided).
      *
      * @param object $caller  Call initiator (used to de-group events).
      * @param string $record  Benchmark record name.
---
+++ source/Spiral/Debug/Traits/BenchmarkTrait.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+namespace Spiral\Debug\Traits;
+
+use Spiral\Core\ContainerInterface;
+use Spiral\Debug\BenchmarkerInterface;
+
+/**
+ * Provides access to benchmark function.
+ */
+trait BenchmarkTrait
+{
+    /**
+     * @var BenchmarkerInterface
+     */
+    private $benchmarker = null;
+
+    /**
+     * @return ContainerInterface
+     */
+    abstract public function container();
+
+    /**
+     * Set custom benchmarker.
+     *
+     * @param BenchmarkerInterface $benchmarker
+     */
+    public function setBenchmarker(BenchmarkerInterface $benchmarker)
+    {
+        $this->benchmarker = $benchmarker;
+    }
+
+    /**
+     * Benchmarks used to record long or important operations inside spiral components. Method should
+     * return elapsed time when record are be closed (same set of arguments has to be provided).
+     *
+     * @param string $record  Benchmark record name.
+     * @param string $context Record context (if any).
+     * @return bool|float
+     */
+    public function benchmark($record, $context = '')
+    {
+        if (empty($this->benchmarker))
+        {
+            if (empty($container = self::container()) || !$container->hasBinding(BenchmarkerInterface::class))
+            {
+                //Nothing to do
+                return false;
+            }
+
+            $this->benchmarker = $container->get(BenchmarkerInterface::class);
+        }
+
+        return $this->benchmarker->benchmark($this, $record, $context);
+    }
+}

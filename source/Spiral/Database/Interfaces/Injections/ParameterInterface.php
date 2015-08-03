7dbcd9699dbe7de38fb0111433c6303f91471587
Wolfy-J <wolfy.jd@gmail.com>
2015-08-04 02:52:54 +0300

    interfaces

--- source/Spiral/Database/Interfaces/Injections/ParameterInterface.php
+++ source/Spiral/Database/Interfaces/Injections/ParameterInterface.php
@@ -8,13 +8,14 @@
  */
 namespace Spiral\Database\Interfaces\Injections;
 
-use Spiral\Database\Injections\SQLFragmentInterface;
-
 /**
- * Parameter interface is very similar to sql fragments, however it can not only mock sql expressions
+ * Parameter interface is very similar to sql fragments, however it may not only mock sql expressions
  * but set of parameters to be injected into this expression.
  *
  * Usually used for complex set of parameters or late parameter binding.
+ *
+ * Database implementation must inject parameter SQL into expression, but use parameter value to be
+ * sent to database.
  */
 interface ParameterInterface extends SQLFragmentInterface
 {

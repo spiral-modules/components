f610e9d9d734b5d9372ca36d515e64bea9a0c2ac
Wolfy-J <wolfy.jd@gmail.com>
2015-09-10 00:32:22 +0300

    CS

--- source/Spiral/ORM/ORM.php
+++ source/Spiral/ORM/ORM.php
@@ -26,7 +26,7 @@ use Spiral\ORM\Exceptions\ORMException;
  * analysis.
  *
  * @todo Think about using views for complex queries?
- * @todo ability to merge multiple tables into one object?
+ * @todo ability to merge multiple tables into one entity - like SearchEntity?
  */
 class ORM extends Singleton
 {

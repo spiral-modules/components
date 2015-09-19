13430e0c5891ff27967a40ad40d893e7936cc84e
Wolfy-J <wolfy.jd@gmail.com>
2015-09-19 15:12:20 +0300

    CS

--- source/Spiral/Files/FilesInterface.php
+++ source/Spiral/Files/FilesInterface.php
@@ -12,7 +12,9 @@ use Spiral\Files\Exceptions\FileNotFoundException;
 use Spiral\Files\Exceptions\WriteErrorException;
 
 /**
- * Access to hard drive or local store.
+ * Access to hard drive or local store. Does not provide full filesystem abstractions.
+ *
+ * @todo Add lock and unlock method.
  */
 interface FilesInterface
 {

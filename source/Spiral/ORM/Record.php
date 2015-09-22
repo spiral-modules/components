e76f2996c6420b168864275e78c7239c3e842645
Wolfy-J <wolfy.jd@gmail.com>
2015-09-22 15:17:37 +0300

    static functions moved to traits

--- source/Spiral/ORM/Record.php
+++ source/Spiral/ORM/Record.php
@@ -15,10 +15,9 @@ use Spiral\Models\ActiveEntityInterface;
 use Spiral\Models\EntityInterface;
 use Spiral\Models\Exceptions\AccessorExceptionInterface;
 use Spiral\Models\SchematicEntity;
-use Spiral\ORM\Entities\Selector;
-use Spiral\ORM\Exceptions\ORMException;
 use Spiral\ORM\Exceptions\RecordException;
 use Spiral\ORM\Exceptions\RelationException;
+use Spiral\ORM\Traits\SelectorTrait;
 use Spiral\Validation\ValidatesInterface;
 
 /**
@@ -31,6 +30,11 @@ use Spiral\Validation\ValidatesInterface;
  */
 class Record extends SchematicEntity implements ActiveEntityInterface
 {
+    /**
+     * Static find functions.
+     */
+    use SelectorTrait;
+
     /**
      * Field format declares how entity must process magic setters and getters. Available values:
      * camelCase, tableize.
@@ -973,77 +977,5 @@ class Record extends SchematicEntity implements ActiveEntityInterface
         $record->setFields($fields)->fire('created');
 
         return $record;
-    }
-
-    /**
-     * Find multiple records based on provided query.
-     *
-     * Example:
-     * User::find(['status' => 'active'], ['profile']);
-     *
-     * @param array|\Closure $where Selection WHERE statement.
-     * @param array          $load  Array or relations to be pre-loaded.
-     * @return Selector
-     */
-    public static function find($where = [], array $load = [])
-    {
-        return static::ormSelector()->load($load)->where($where);
-    }
-
-    /**
-     * Fetch one record based on provided query or return null. Use second argument to specify
-     * relations to be loaded.
-     *
-     * Example:
-     * User::findOne(['name' => 'Wolfy-J'], ['profile'], ['id' => 'DESC']);
-     *
-     * @param array|\Closure $where   Selection WHERE statement.
-     * @param array          $load    Array or relations to be pre-loaded.
-     * @param array          $orderBy Sort by conditions.
-     * @return Record|null
-     */
-    public static function findOne($where = [], array $load = [], array $orderBy = [])
-    {
-        $selector = static::find($where, $load);
-        foreach ($orderBy as $column => $direction) {
-            $selector->orderBy($column, $direction);
-        }
-
-        return $selector->findOne();
-    }
-
-    /**
-     * Find record using it's primary key. Relation data can be preloaded with found record.
-     *
-     * Example:
-     * User::findByID(1, ['profile']);
-     *
-     * @param mixed $primaryKey Primary key.
-     * @param array $load       Array or relations to be pre-loaded.
-     * @return Record|null
-     */
-    public static function findByPK($primaryKey, array $load = [])
-    {
-        return static::ormSelector()->load($load)->findByPK($primaryKey);
-    }
-
-    /**
-     * Instance of ORM Selector associated with specific document.
-     *
-     * @see   Component::staticContainer()
-     * @param ORM $orm ORM component, global container will be called if not instance provided.
-     * @return Selector
-     * @throws ORMException
-     * @event selector(Selector $selector)
-     */
-    public static function ormSelector(ORM $orm = null)
-    {
-        //Ensure traits
-        static::initialize();
-
-        //Using global container as fallback
-        $orm = self::saturate($orm, ORM::class);
-
-        return static::events()->fire('selector', $orm->ormSelector(static::class));
     }
 }
---
+++ source/Spiral/ORM/Traits/SelectorTrait.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ * @copyright ©2009-2015
+ */
+
+namespace Spiral\ORM\Traits;
+
+use Spiral\ORM\Entities\Selector;
+use Spiral\ORM\Exceptions\ORMException;
+use Spiral\ORM\ORM;
+use Spiral\ORM\Record;
+
+/**
+ * Static find record functionality.
+ */
+trait SelectorTrait
+{
+    /**
+     * Find multiple records based on provided query.
+     *
+     * Example:
+     * User::find(['status' => 'active'], ['profile']);
+     *
+     * @param array|\Closure $where Selection WHERE statement.
+     * @param array          $load  Array or relations to be pre-loaded.
+     * @return Selector
+     */
+    public static function find($where = [], array $load = [])
+    {
+        return static::ormSelector()->load($load)->where($where);
+    }
+
+    /**
+     * Fetch one record based on provided query or return null. Use second argument to specify
+     * relations to be loaded.
+     *
+     * Example:
+     * User::findOne(['name' => 'Wolfy-J'], ['profile'], ['id' => 'DESC']);
+     *
+     * @param array|\Closure $where   Selection WHERE statement.
+     * @param array          $load    Array or relations to be pre-loaded.
+     * @param array          $orderBy Sort by conditions.
+     * @return Record|null
+     */
+    public static function findOne($where = [], array $load = [], array $orderBy = [])
+    {
+        $selector = static::find($where, $load);
+        foreach ($orderBy as $column => $direction) {
+            $selector->orderBy($column, $direction);
+        }
+
+        return $selector->findOne();
+    }
+
+    /**
+     * Find record using it's primary key. Relation data can be preloaded with found record.
+     *
+     * Example:
+     * User::findByID(1, ['profile']);
+     *
+     * @param mixed $primaryKey Primary key.
+     * @param array $load       Array or relations to be pre-loaded.
+     * @return Record|null
+     */
+    public static function findByPK($primaryKey, array $load = [])
+    {
+        return static::ormSelector()->load($load)->findByPK($primaryKey);
+    }
+
+    /**
+     * Instance of ORM Selector associated with specific document.
+     *
+     * @see   Component::staticContainer()
+     * @param ORM $orm ORM component, global container will be called if not instance provided.
+     * @return Selector
+     * @throws ORMException
+     * @event selector(Selector $selector)
+     */
+    public static function ormSelector(ORM $orm = null)
+    {
+        //Ensure traits
+        static::initialize();
+
+        //Using global container as fallback
+        $orm = self::saturate($orm, ORM::class);
+
+        return static::events()->fire('selector', $orm->ormSelector(static::class));
+    }
+}

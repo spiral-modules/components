af4bc69222a9d8ab8dd29171018dd2e3425c3ad9
Anton Titov <wolfy.jd@gmail.com>
2015-12-04 15:30:05 +0300

    Update FilesInterface.php

--- source/Spiral/Files/FilesInterface.php
+++ source/Spiral/Files/FilesInterface.php
@@ -12,6 +12,8 @@ use Spiral\Files\Exceptions\WriteErrorException;
 
 /**
  * Access to hard drive or local store. Does not provide full filesystem abstractions.
+ * 
+ * @todo Add more methods when needed.
  */
 interface FilesInterface
 {

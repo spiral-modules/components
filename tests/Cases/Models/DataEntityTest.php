12be0a05c6c213d1a83965deca9e21d233ee746c
Wolfy-J <wolfy.jd@gmail.com>
2015-12-05 17:46:51 +0300

    few more tests

--- tests/Cases/Models/DataEntityTest.php
+++ tests/Cases/Models/DataEntityTest.php
@@ -42,11 +42,7 @@ class DataEntityTest extends \PHPUnit_Framework_TestCase
 
     public function testSerialize()
     {
-        $data = [
-            'a' => 123,
-            'b' => null,
-            'c' => 'test'
-        ];
+        $data = ['a' => 123, 'b' => null, 'c' => 'test'];
 
         $entity = new DataEntity($data);
         $this->assertEquals($data, $entity->serializeData());

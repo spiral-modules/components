d8bdad0d42f8eb863d88f2c005bd04602c31cd59
Wolfy-J <wolfy.jd@gmail.com>
2015-12-06 23:54:32 +0300

    container moved id

--- source/Spiral/Core/Container/SingletonInterface.php
+++ source/Spiral/Core/Container/SingletonInterface.php
@@ -10,6 +10,8 @@ namespace Spiral\Core\Container;
 /**
  * Class treated as singleton MAY be saved as reference in IoC bindings - this is spiral Container
  * specific class. Must declare SINGLETON constant.
+ *
+ * @todo potentially deprecated
  */
 interface SingletonInterface
 {

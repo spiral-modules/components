7acef7f1ff7966686fb951fd394a8cfccc2930d8
Wolfy-J <wolfy.jd@gmail.com>
2015-12-28 18:24:56 +0300

    topological sort

--- source/Spiral/Database/Entities/SynchronizationBus.php
+++ source/Spiral/Database/Entities/SynchronizationBus.php
@@ -11,7 +11,7 @@ namespace Spiral\Database\Entities;
 use Spiral\Core\Component;
 use Spiral\Database\Entities\Schemas\AbstractTable;
 use Spiral\Debug\Traits\LoggerTrait;
-use Spiral\Sorters\DFSSorter;
+use Spiral\Support\DFSSorter;
 
 /**
  * Saves multiple linked tables at once but treating their cross dependency.
@@ -58,10 +58,13 @@ class SynchronizationBus extends Component
     public function sortedTables()
     {
         $tables = $this->tables;
-        $sorter = new DFSSorter();
 
+        /*
+         * Tables has to be sorted using topological graph to execute operations in a valid order.
+         */
+        $sorter = new DFSSorter();
         foreach ($tables as $table) {
-            $sorter->addItemObject($table->getName(), $table, $table->getDependencies());
+            $sorter->addItem($table->getName(), $table, $table->getDependencies());
         }
 
         return $sorter->sort();

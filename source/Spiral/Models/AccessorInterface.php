1c39448dd7ca632eda009546960d47ff1ec78dcb
Wolfy-J <wolfy.jd@gmail.com>
2016-12-06 17:42:28 +0300

    minus parent dependency

--- source/Spiral/Models/AccessorInterface.php
+++ source/Spiral/Models/AccessorInterface.php
@@ -13,22 +13,9 @@ use Spiral\Validation\ValueInterface;
 
 /**
  * Accessors used to mock access to model field, control value setting, serializing and etc.
- *
- * @todo rework parent embedding
  */
 interface AccessorInterface extends ValueInterface, \JsonSerializable
 {
-    /**
-     * Must embed accessor to another parent model. Allowed to clone itself.
-     *
-     * @param EntityInterface $parent
-     *
-     * @return static
-     *
-     * @throws AccessorExceptionInterface
-     */
-    public function embed(EntityInterface $parent);
-
     /**
      * Change mocked data.
      *

9360d41ed3a95b2ab187bd33f0dc1d760cff0835
Wolfy-J <wolfy.jd@gmail.com>
2016-12-07 14:05:20 +0300

    CS and comments

--- source/Spiral/Core/Component.php
+++ source/Spiral/Core/Component.php
@@ -45,8 +45,8 @@ abstract class Component
          * Technically your code can work without ever being using fallback container, all
          * spiral components can do that and only few traits will require few extra parameters.
          *
-         * I'm planning one day to run spiral as worker and later in async mode (few app processes
-         * in memory), memory schemas might help a lot by minimizing runtime code by using
+         * I'm planning one day (update: DONE) to run spiral as worker and later in async mode (few app
+         * processes in memory), memory schemas might help a lot by minimizing runtime code by using
          * pre-calculated behaviours.
          */
 

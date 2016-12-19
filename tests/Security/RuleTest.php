c6b6587fd1da45ca1eb5fdb8a546d3a4ef00376e
Wolfy-J <wolfy.jd@gmail.com>
2016-12-19 16:27:03 +0300

    security tests initial

---
+++ tests/Security/RuleTest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * components
+ *
+ * @author    Wolfy-J
+ */
+namespace Security;
+
+use Spiral\Core\ResolverInterface;
+use Spiral\Security\ActorInterface;
+use Spiral\Security\Rule;
+
+/**
+ * Class RuleTest
+ *
+ * @package Spiral\Security
+ */
+class RuleTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|ResolverInterface
+     */
+    private $resolver;
+
+    /**
+     * @var \PHPUnit_Framework_MockObject_MockObject|Rule
+     */
+    private $rule;
+
+    protected function setUp()
+    {
+        $this->resolver = $this->createMock(ResolverInterface::class);
+        $this->rule = $this->getMockBuilder(Rule::class)
+            ->setConstructorArgs([$this->resolver])
+            ->setMethods([Rule::CHECK_METHOD])->getMock();
+    }
+
+    /**
+     * @param $permission
+     * @param $context
+     * @param $allowed
+     *
+     * @dataProvider allowsProvider
+     */
+    public function testAllows($permission, $context, $allowed)
+    {
+        /** @var ActorInterface $actor */
+        $actor = $this->createMock(ActorInterface::class);
+        $parameters = [
+                'actor'      => $actor,
+                'user'       => $actor,
+                'permission' => $permission,
+                'context'    => $context,
+            ] + $context;
+        $method = new \ReflectionMethod($this->rule, Rule::CHECK_METHOD);
+        $this->resolver
+            ->expects($this->once())
+            ->method('resolveArguments')
+            ->with($method, $parameters)
+            ->willReturn([$parameters]);
+        $this->rule
+            ->expects($this->once())
+            ->method(Rule::CHECK_METHOD)
+            ->with($parameters)
+            ->willReturn($allowed);
+        $this->assertEquals($allowed, $this->rule->allows($actor, $permission, $context));
+    }
+
+    /**
+     * @return array
+     */
+    public function allowsProvider()
+    {
+        return [
+            ['foo.create', [], false],
+            ['foo.create', [], true],
+            ['foo.create', ['a' => 'b'], false],
+            ['foo.create', ['a' => 'b'], true],
+        ];
+    }
+}

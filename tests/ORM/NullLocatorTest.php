16db6ee75d9c3e74403b1099ebc6cd38ae03dd13
Wolfy-J <wolfy.jd@gmail.com>
2017-01-05 00:42:08 +0300

    a bit more tests

---
+++ tests/ORM/NullLocatorTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * components
+ *
+ * @author    Wolfy-J
+ */
+namespace Spiral\Tests\ORM;
+
+use Spiral\ORM\Schemas\NullLocator;
+
+class NullLocatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLocator()
+    {
+        $locator = new NullLocator();
+
+        $this->assertSame([], $locator->locateSchemas());
+        $this->assertSame([], $locator->locateSources());
+    }
+}

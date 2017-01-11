2ed6c7e82f97cec32e6a43f828fa3f7832c0c0cf
Wolfy-J <wolfy.jd@gmail.com>
2017-01-11 23:52:53 +0300

    selectors, iterators and etc

--- source/Spiral/ORM/Entities/RecordIterator.php
+++ source/Spiral/ORM/Entities/RecordIterator.php
@@ -6,7 +6,74 @@
  */
 namespace Spiral\ORM\Entities;
 
-class RecordIterator
-{
+use Spiral\Models\EntityInterface;
+use Spiral\ORM\ORMInterface;
 
+/**
+ * Instantiates array of entities. At this moment implementation is rather simple.
+ *
+ * @todo upgrade to \IteratorIterator?
+ */
+class RecordIterator implements \IteratorAggregate
+{
+    /**
+     * Array of entity data to be fed into instantiators.
+     *
+     * @var array
+     */
+    private $data = [];
+
+    /**
+     * @var EntityInterface[]
+     */
+    private $entities = [];
+
+    /**
+     * Class to be instantiated.
+     *
+     * @var string
+     */
+    private $class;
+
+    /**
+     * Responsible for entity construction.
+     *
+     * @invisible
+     * @var ORMInterface
+     */
+    private $orm;
+
+    /**
+     * @param array        $data
+     * @param string       $class
+     * @param ORMInterface $orm
+     */
+    public function __construct(array $data, string $class, ORMInterface $orm)
+    {
+        $this->data = $data;
+        $this->class = $class;
+        $this->orm = $orm;
+    }
+
+    /**
+     * @return \ArrayIterator
+     */
+    public function getIterator()
+    {
+        if (empty($this->entities)) {
+            foreach ($this->data as $data) {
+                /*
+                 * Mass entity initialization.
+                 */
+                $this->entities[] = $this->orm->make(
+                    $this->class,
+                    $data,
+                    ORMInterface::STATE_LOADED,
+                    true
+                );
+            }
+        }
+
+        return new \ArrayIterator($this->entities);
+    }
 }

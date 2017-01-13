1d6861c6bf4f024c6ce3112e41a0f37445b11261
Wolfy-J <wolfy.jd@gmail.com>
2017-01-13 21:10:36 +0300

    fix key

--- source/Spiral/ORM/Helpers/RelationOptions.php
+++ source/Spiral/ORM/Helpers/RelationOptions.php
@@ -149,7 +149,7 @@ class RelationOptions
             'source:table'      => $source->getTable(),
 
             //Parent record primary key
-            'source:primaryKey' => $source->getPrimary(),
+            'source:primaryKey' => $source->getPrimary()->getName(),
         ];
 
         //Some options may use values declared in other definition fields

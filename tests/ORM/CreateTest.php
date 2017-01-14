335ae7f9aee483a2af566b02db71528feb8f9550
Wolfy-J <wolfy.jd@gmail.com>
2017-01-14 17:30:00 +0300

    first tests

--- tests/ORM/CreateTest.php
+++ tests/ORM/CreateTest.php
@@ -6,14 +6,43 @@
  */
 namespace Spiral\Tests\ORM;
 
+use Spiral\ORM\Transaction;
 use Spiral\Tests\ORM\Fixtures\User;
 
 abstract class CreateTest extends BaseTest
 {
-    public function testCreate()
+    public function testNotLoaded()
     {
         /** @var User $user */
         $user = $this->orm->make(User::class);
         $this->assertFalse($user->isLoaded());
+    }
+
+    public function testSaveActiveRecordAndCheckLoaded()
+    {
+        /** @var User $user */
+        $user = $this->orm->make(User::class);
+        $this->assertFalse($user->isLoaded());
+
+        $user->save();
+        $this->assertTrue($user->isLoaded());
+        $this->assertNotEmpty($user->primaryKey());
+    }
+
+    public function testStoreInTransactionAndCheckLoaded()
+    {
+        /** @var User $user */
+        $user = $this->orm->make(User::class);
+        $this->assertFalse($user->isLoaded());
+
+        $transaction = new Transaction();
+        $transaction->store($user);
+
+        $this->assertTrue($user->isLoaded());
+        $this->assertEmpty($user->primaryKey());
+        $transaction->run();
+
+        $this->assertTrue($user->isLoaded());
+        $this->assertNotEmpty($user->primaryKey());
     }
 }

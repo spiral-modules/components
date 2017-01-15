999a239f34ee6858f4f94a5f1fa3289fb8e3d286
Wolfy-J <wolfy.jd@gmail.com>
2017-01-15 04:39:29 +0300

    cs

--- tests/ORM/BelongsToRelationTest.php
+++ tests/ORM/BelongsToRelationTest.php
@@ -341,9 +341,6 @@ abstract class BelongsToRelationTest extends BaseTest
         $this->assertFalse($recursive->parent->parent->getRelations()->get('parent')->isLoaded());
     }
 
-    /**
-     * Fun.
-     */
     public function testLoadOptionalFirstLevelOfParentsInload()
     {
         $recursive = new Recursive();

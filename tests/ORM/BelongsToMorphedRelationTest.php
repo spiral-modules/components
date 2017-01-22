b2ffd37395a99429e4bf48b39ee86e8576e171d5
Wolfy-J <wolfy.jd@gmail.com>
2017-01-22 20:02:34 +0300

    tests

--- tests/ORM/BelongsToMorphedRelationTest.php
+++ tests/ORM/BelongsToMorphedRelationTest.php
@@ -28,171 +28,171 @@ abstract class BelongsToMorphedRelationTest extends BaseTest
         Label::class
     ];
 
-//    public function testSchemaBuilding()
-//    {
-//        $picture = new Picture();
-//        $this->assertTrue($picture->hasField('parent_id'));
-//        $this->assertTrue($picture->hasField('parent_type'));
-//    }
-//
-//    public function testSetParent()
-//    {
-//        $picture = new Picture();
-//        $picture->parent = $user = new User();
-//        $picture->save();
-//
-//        $this->assertSameInDB($picture);
-//
-//        $this->assertEquals('user', $picture->parent_type);
-//        $this->assertEquals($user->primaryKey(), $picture->parent_id);
-//    }
-//
-//    public function testChangeParent()
-//    {
-//        $picture = new Picture();
-//        $picture->parent = $user = new User();
-//        $picture->save();
-//
-//        $this->assertSameInDB($picture);
-//
-//        $this->assertEquals('user', $picture->parent_type);
-//        $this->assertEquals($user->primaryKey(), $picture->parent_id);
-//
-//        $picture->parent = $post = new Post();
-//        $picture->parent->author = $user;
-//        $picture->save();
-//
-//        $this->assertSameInDB($picture);
-//
-//        $this->assertEquals('post', $picture->parent_type);
-//        $this->assertEquals($post->primaryKey(), $picture->parent_id);
-//    }
-//
-//    public function testLazyLoad()
-//    {
-//        $picture = new Picture();
-//        $picture->parent = $user = new User();
-//        $picture->save();
-//
-//        $picture = $this->orm->source(Picture::class)->findByPK($picture->primaryKey());
-//        $this->assertSimilar($user, $picture->parent);
-//
-//        $picture->parent = $post = new Post();
-//        $picture->parent->author = $user;
-//        $picture->save();
-//
-//        $picture = $this->orm->source(Picture::class)->findByPK($picture->primaryKey());
-//        $this->assertSimilar($post, $picture->parent);
-//    }
-//
-//    public function testSetNull()
-//    {
-//        $picture = new Picture();
-//        $picture->parent = $user = new User();
-//        $picture->save();
-//
-//        $picture = $this->orm->source(Picture::class)->findByPK($picture->primaryKey());
-//        $this->assertSimilar($user, $picture->parent);
-//
-//        $picture->parent = null;
-//        $picture->save();
-//
-//        $picture = $this->orm->source(Picture::class)->findByPK($picture->primaryKey());
-//        $this->assertSame(null, $picture->parent);
-//    }
-//
-//    public function testInversedLazy()
-//    {
-//        $picture = new Picture();
-//        $picture->parent = $user = new User();
-//        $picture->save();
-//
-//        $user = $this->orm->source(User::class)->findByPK($user->primaryKey());
-//
-//        $this->assertSimilar($picture, $user->picture);
-//
-//        $picture->parent = $post = new Post();
-//        $picture->parent->author = $user;
-//        $picture->save();
-//
-//        $post = $this->orm->source(Post::class)->findByPK($user->primaryKey());
-//        $this->assertSimilar($picture, $post->picture);
-//    }
-//
-//    public function testInversedPostload()
-//    {
-//        $picture = new Picture();
-//        $picture->parent = $user = new User();
-//        $picture->save();
-//
-//        $user = $this->orm->selector(User::class)->wherePK($user->primaryKey())
-//            ->load('picture', ['method' => RelationLoader::POSTLOAD])
-//            ->findOne();
-//
-//        $this->assertTrue($user->getRelations()->get('picture')->isLoaded());
-//
-//        $this->assertSimilar($picture, $user->picture);
-//
-//        $picture = new Picture();
-//        $picture->parent = $post = new Post();
-//        $picture->parent->author = $user;
-//        $picture->save();
-//
-//        $post = $this->orm->selector(Post::class)->wherePK($post->primaryKey())
-//            ->load('picture', ['method' => RelationLoader::POSTLOAD])
-//            ->findOne();
-//
-//        $this->assertTrue($post->getRelations()->get('picture')->isLoaded());
-//        $this->assertSimilar($picture, $post->picture);
-//    }
-//
-//    public function testInversedInload()
-//    {
-//        $picture = new Picture();
-//        $picture->parent = $user = new User();
-//        $picture->save();
-//
-//        $user = $this->orm->selector(User::class)->wherePK($user->primaryKey())
-//            ->with('picture')
-//            ->load('picture', ['method' => RelationLoader::INLOAD])
-//            ->findOne();
-//
-//        $this->assertTrue($user->getRelations()->get('picture')->isLoaded());
-//
-//        $this->assertSimilar($picture, $user->picture);
-//
-//        $picture = new Picture();
-//        $picture->parent = $post = new Post();
-//        $picture->parent->author = $user;
-//        $picture->save();
-//
-//        $post = $this->orm->selector(Post::class)->wherePK($post->primaryKey())
-//            ->with('picture')
-//            ->load('picture', ['method' => RelationLoader::INLOAD])
-//            ->findOne();
-//
-//        $this->assertTrue($post->getRelations()->get('picture')->isLoaded());
-//        $this->assertSimilar($picture, $post->picture);
-//    }
+    public function testSchemaBuilding()
+    {
+        $picture = new Picture();
+        $this->assertTrue($picture->hasField('parent_id'));
+        $this->assertTrue($picture->hasField('parent_type'));
+    }
 
-//    public function testMorphedMany()
-//    {
-//        $user = new User();
-//        $user->labels->add($l1 = new Label(['name' => 'A']));
-//        $user->labels->add($l2 = new Label(['name' => 'B']));
-//        $user->save();
-//
-//        $post = new Post();
-//        $post->author = $user;
-//        $post->labels->add($l3 = new Label(['name' => 'C']));
-//        $post->save();
-//
-//        $this->assertSameInDB($user);
-//        $this->assertSameInDB($post);
-//        $this->assertSameInDB($l1);
-//        $this->assertSameInDB($l2);
-//        $this->assertSameInDB($l3);
-//    }
+    public function testSetParent()
+    {
+        $picture = new Picture();
+        $picture->parent = $user = new User();
+        $picture->save();
+
+        $this->assertSameInDB($picture);
+
+        $this->assertEquals('user', $picture->parent_type);
+        $this->assertEquals($user->primaryKey(), $picture->parent_id);
+    }
+
+    public function testChangeParent()
+    {
+        $picture = new Picture();
+        $picture->parent = $user = new User();
+        $picture->save();
+
+        $this->assertSameInDB($picture);
+
+        $this->assertEquals('user', $picture->parent_type);
+        $this->assertEquals($user->primaryKey(), $picture->parent_id);
+
+        $picture->parent = $post = new Post();
+        $picture->parent->author = $user;
+        $picture->save();
+
+        $this->assertSameInDB($picture);
+
+        $this->assertEquals('post', $picture->parent_type);
+        $this->assertEquals($post->primaryKey(), $picture->parent_id);
+    }
+
+    public function testLazyLoad()
+    {
+        $picture = new Picture();
+        $picture->parent = $user = new User();
+        $picture->save();
+
+        $picture = $this->orm->source(Picture::class)->findByPK($picture->primaryKey());
+        $this->assertSimilar($user, $picture->parent);
+
+        $picture->parent = $post = new Post();
+        $picture->parent->author = $user;
+        $picture->save();
+
+        $picture = $this->orm->source(Picture::class)->findByPK($picture->primaryKey());
+        $this->assertSimilar($post, $picture->parent);
+    }
+
+    public function testSetNull()
+    {
+        $picture = new Picture();
+        $picture->parent = $user = new User();
+        $picture->save();
+
+        $picture = $this->orm->source(Picture::class)->findByPK($picture->primaryKey());
+        $this->assertSimilar($user, $picture->parent);
+
+        $picture->parent = null;
+        $picture->save();
+
+        $picture = $this->orm->source(Picture::class)->findByPK($picture->primaryKey());
+        $this->assertSame(null, $picture->parent);
+    }
+
+    public function testInversedLazy()
+    {
+        $picture = new Picture();
+        $picture->parent = $user = new User();
+        $picture->save();
+
+        $user = $this->orm->source(User::class)->findByPK($user->primaryKey());
+
+        $this->assertSimilar($picture, $user->picture);
+
+        $picture->parent = $post = new Post();
+        $picture->parent->author = $user;
+        $picture->save();
+
+        $post = $this->orm->source(Post::class)->findByPK($user->primaryKey());
+        $this->assertSimilar($picture, $post->picture);
+    }
+
+    public function testInversedPostload()
+    {
+        $picture = new Picture();
+        $picture->parent = $user = new User();
+        $picture->save();
+
+        $user = $this->orm->selector(User::class)->wherePK($user->primaryKey())
+            ->load('picture', ['method' => RelationLoader::POSTLOAD])
+            ->findOne();
+
+        $this->assertTrue($user->getRelations()->get('picture')->isLoaded());
+
+        $this->assertSimilar($picture, $user->picture);
+
+        $picture = new Picture();
+        $picture->parent = $post = new Post();
+        $picture->parent->author = $user;
+        $picture->save();
+
+        $post = $this->orm->selector(Post::class)->wherePK($post->primaryKey())
+            ->load('picture', ['method' => RelationLoader::POSTLOAD])
+            ->findOne();
+
+        $this->assertTrue($post->getRelations()->get('picture')->isLoaded());
+        $this->assertSimilar($picture, $post->picture);
+    }
+
+    public function testInversedInload()
+    {
+        $picture = new Picture();
+        $picture->parent = $user = new User();
+        $picture->save();
+
+        $user = $this->orm->selector(User::class)->wherePK($user->primaryKey())
+            ->with('picture')
+            ->load('picture', ['method' => RelationLoader::INLOAD])
+            ->findOne();
+
+        $this->assertTrue($user->getRelations()->get('picture')->isLoaded());
+
+        $this->assertSimilar($picture, $user->picture);
+
+        $picture = new Picture();
+        $picture->parent = $post = new Post();
+        $picture->parent->author = $user;
+        $picture->save();
+
+        $post = $this->orm->selector(Post::class)->wherePK($post->primaryKey())
+            ->with('picture')
+            ->load('picture', ['method' => RelationLoader::INLOAD])
+            ->findOne();
+
+        $this->assertTrue($post->getRelations()->get('picture')->isLoaded());
+        $this->assertSimilar($picture, $post->picture);
+    }
+
+    public function testMorphedMany()
+    {
+        $user = new User();
+        $user->labels->add($l1 = new Label(['name' => 'A']));
+        $user->labels->add($l2 = new Label(['name' => 'B']));
+        $user->save();
+
+        $post = new Post();
+        $post->author = $user;
+        $post->labels->add($l3 = new Label(['name' => 'C']));
+        $post->save();
+
+        $this->assertSameInDB($user);
+        $this->assertSameInDB($post);
+        $this->assertSameInDB($l1);
+        $this->assertSameInDB($l2);
+        $this->assertSameInDB($l3);
+    }
 
     public function testMorphedManyLazy()
     {

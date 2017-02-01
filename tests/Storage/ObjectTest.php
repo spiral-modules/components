84e7485e5eb58f53a53341b9acce449d55aa51b8
Wolfy-J <wolfy.jd@gmail.com>
2017-02-01 20:43:18 +0300

    better exceptions in storage

--- tests/Storage/ObjectTest.php
+++ tests/Storage/ObjectTest.php
@@ -44,7 +44,7 @@ abstract class ObjectTest extends BaseTest
     }
 
     /**
-     * @expectedException \Spiral\Storage\Exceptions\ObjectException
+     * @expectedException \Spiral\Storage\Exceptions\BucketException
      * @expectedExceptionMessage Source must be a valid resource, stream or filename, invalid value
      *                           given
      */
